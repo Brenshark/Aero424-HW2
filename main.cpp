425570097be60abf1fc8767ff7e17d8ee2b3d413
Brenshark <brensharkg@gmail.com>
2023-11-07 23:59:04 -0500

    Question 3 & 4 added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <map>
 #include <iostream>
 #include <string>
+#include <HW2_Visualizer.h>
+
 using namespace std;
 
 void setvel(double vel) {
 	this->vel = vel;
-}
+};
 
 void setloiter(double loiter_time) {
 	this->loiter_time = loiter_time;
+};
+
+void Plane::operate(double dt) {
+	if (loiter_time != 0) loiter_time -= dt;
+	else if (wait_time != 0) wait_time -= dt;
+	else if (pos < distance) {
+		pos += vel * dt; at_SCE = 0;
+	}
+	else if (destination == "SCES") at_SCE = 1;
+
+	wait_time = time_on_ground();
+	destination = origin;
+	pos = 0.0;
+};
+double Plane::distance_to_SCE() {
+	if (destination == "SCE") return distance - pos;
+	else return 1000;
+};
+
+string plane_type() {
+	return "GA";
+}
+
+double draw_from_normal_dist(double mean, double standard_deviation){
+	random_device rd{};
+	mt19937 gen{ rd() };
+	normal_distribution<> d{ mean,standard_deviation };
+	return d(gen);
 }
 
 class Plane {
@@ -61,7 +91,32 @@ public:
 	double wait_time = 0;
 	double loiter_time = 0;
 	bool at_SCE = 0;
+};
+class Airliner {
+private:
+	string Airline;
+public:
+	const Airliner(string Airline, string from, string to) {
+		string plane_type();
+		double time_on_ground();
+	}
+	Airliner(string Airline, string from, string to) {
+		this->Airline = Airline;
+	}
+	string plane_type() { return Airline; }
+	double time_on_ground(double wait_time) {
+		wait_time = draw_from_normal_dist(1800, 600);
+		return wait_time;
+	}
+};
 
-
-
+class GeneralAviation {
+public:
+	GeneralAviation(string from, string to);
+	double time_on_ground() {
+		double wait_time = draw_from_normal_dist(600, 60);
+		return wait_time;
+	}
+	double time_on_ground();
+	
 };
