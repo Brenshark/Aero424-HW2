cc3c238db4c181ac95bc9a7df9805a924ff86296
Brenshark <brensharkg@gmail.com>
2023-11-10 22:30:32 -0500

    Updated organization

--- main.cpp
+++ main.cpp
@@ -5,12 +5,81 @@
 #include "HW2_Visualizer.h"
 #include <math.h>
 #include <random>
+#include "SDL.h"
+#include <vector>
 
 using namespace std;
 
+class Plane {
+protected:
+	double wait_time;
+
+private:
+	double pos;
+	double vel;
+	double distance;
+	double loiter_time;
+	bool at_SCE;
+	string origin;
+	string destination;
+
+	map<pair<string, string>, double> flight_dist{
+		{make_pair("SCE","PHL"),160},
+		{make_pair("SCE","ORD"),640},
+		{make_pair("SCE","EWR"),220}
+	};
+
+public:
+	Plane(string from, string to);
+	virtual ~Plane();
+	void operate(double dt);
+	double getpos();
+	double getvel();
+	double getloiter();
+	string getorigin();
+	string getdestination();
+	bool getat_SCE();
+
+	void setvel(double vel);
+	void setloiter(double loiter_time);
+	double distance_to_SCE();
+
+	virtual double time_on_ground() = 0;
+	virtual string plane_type();
+	static double draw_from_normal_dist(double mean, double standard_deviation);
+};
+
+class Airliner : public Plane{
+private:
+	string Airline;
+public:
+	Airliner(string Airline, string from, string to);
+	~Airliner();
+	string plane_type();
+	double time_on_ground();
+};
+
+class GeneralAviation : public Plane{
+public:
+	GeneralAviation(string from, string to);
+	~GeneralAviation();
+	double time_on_ground();
+};
+
+class ATC {
+private:
+	
+public:
+	ATC();
+	~ATC();
+
+};
+ 
+//Question 3 {
 double Plane::getpos() { 
 	return pos; 
 }
+
 double Plane::getvel() {
 	return vel;
 }
@@ -31,112 +100,88 @@ bool Plane::getat_SCE() {
 	return at_SCE;
 }
 
-void setvel(double vel) {
-	vel = vel;
+void Plane::setvel(double vel) {
+	this->vel = vel;
 };
 
-void setloiter(double loiter_time) {
-	loiter_time = loiter_time;
+void Plane::setloiter(double loiter_time) {
+	this->loiter_time = loiter_time;
 };
 
 void Plane::operate(double dt) {
-	if (loiter_time != 0) loiter_time -= dt;
-	else if (wait_time != 0) wait_time -= dt;
+	if (loiter_time != 0) {
+		loiter_time -= dt;
+		if (loiter_time < 0) loiter_time = 0;
+		return;
+	}
+	else if (wait_time != 0) {
+		wait_time -= dt;
+		if (wait_time < 0) wait_time = 0;
+		return;
+	}
 	else if (pos < distance) {
 		pos += vel * dt; at_SCE = 0;
+		return;
 	}
-	else if (destination == "SCES") at_SCE = 1;
+	else if (destination == "SCE") at_SCE = 1;
 
 	wait_time = time_on_ground();
 	destination = origin;
 	pos = 0.0;
 };
+
+GeneralAviation::GeneralAviation(string from, string to) :Plane(from, to) {}
+GeneralAviation::~GeneralAviation() {}
+double GeneralAviation::time_on_ground() {
+	wait_time = draw_from_normal_dist(1800, 600);
+	return wait_time;
+}
+
+Plane::Plane(string from, string to) {
+	origin = from;
+	destination = to;
+	if (origin == "SCE")
+		distance = this->flight_dist[{origin, destination}];
+	else distance = this->flight_dist[{destination, origin}];
+	pos = 0;
+	vel = 0;
+	wait_time = 0;
+	loiter_time = 0;
+	at_SCE = 0;
+}
+
+Plane::~Plane() {}
+
 double Plane::distance_to_SCE() {
 	if (destination == "SCE") return distance - pos;
 	else return 1000;
 };
 
-string plane_type() {
+string Plane::plane_type() {
 	return "GA";
 }
 
-double draw_from_normal_dist(double mean, double standard_deviation){
+double Plane::draw_from_normal_dist(double mean, double standard_deviation){
 	random_device rd{};
 	mt19937 gen{ rd() };
 	normal_distribution<> d{ mean,standard_deviation };
 	return d(gen);
 }
 
-class Plane{
-protected:
-	double wait_time;
+Airliner::Airliner(string Airline, string from, string to) :Plane(from, to){
+	this->Airline = Airline;
+}
+Airliner::~Airliner() {}
+string Airliner::plane_type() {
+	return Airline;
+}
 
-private:
-	double pos;
-	double vel;
-	double distance;
-	double loiter_time;
-	bool at_SCE;
-	string origin;
-	string destination;
-	
-	map<pair<string, string>, double> flight_dist{
-		{make_pair("SCE","PHL"),160},
-		{make_pair("SCE","ORD"),640},
-		{make_pair("SCE","EWR"),220}
-	};
-public:
-	Plane(string from, string to);
-	virtual ~Plane();
-	void operate(double dt);
-	double getpos();
-	double getvel();
-	double getloiter();
-	string getorigin();
-	string getdestination();
-	bool getat_SCE();
+double Airliner::time_on_ground(){
+	wait_time = draw_from_normal_dist(1800, 600);
+	return wait_time;
+}
 
-	void setvel(double vel);
-	void setloiter(double loiter_time);
-	double distance_to_SCE();
+int main() {
 
-	virtual double time_on_ground() = 0;
-	virtual string plane_type();
-	static double draw_from_normal_dist(double mean, double standard_deviation);
-
-	const Plane(string from, string to);
-	string getorigin();
-	string getdestination();
-	string from;
-	string to;
-	string origin = from;
-	string destination = to;
-};
-class Airliner {
-private:
-	string Airline;
-public:
-	const Airliner(string Airline, string from, string to) {
-		string plane_type();
-		double time_on_ground();
-	}
-	Airliner(string Airline, string from, string to) {
-		this->Airline = Airline;
-	}
-	string plane_type() { return Airline; }
-	double time_on_ground(double wait_time) {
-		wait_time = draw_from_normal_dist(1800, 600);
-		return wait_time;
-	}
-};
-
-class GeneralAviation {
-public:
-	GeneralAviation(string from, string to);
-	double time_on_ground() {
-		double wait_time = draw_from_normal_dist(600, 60);
-		return wait_time;
-	}
-	double time_on_ground();
-	
-};
+	return 0;
+}
